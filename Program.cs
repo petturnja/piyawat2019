2f2d25d1d996780df6108f0fade886b932f07380
piyawat sairod <petturnj999@gmail.com>
2019-07-26 20:42:07 +0700

    addpro3

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3._1
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            
+                string[][] customers = new string[5][];
+                for (int i = 0; i < 5; i++)
+                {
+                    customers[i] = new string[3];
+                    Console.WriteLine("No. : " + (i + 1));
+                    Console.WriteLine("Input Name :" + (i + 1) + ".");
+                    customers[i][0] = Console.ReadLine();
+                    Console.WriteLine("Input Address :" + (i + 1) + ".");
+                    customers[i][1] = Console.ReadLine();
+                    Console.WriteLine("Input Telephone :" + (i + 1) + ".");
+                    customers[i][2] = Console.ReadLine();
+                    if (i == 4)
+                    {
+                        Console.WriteLine("Complete");
+                        Console.ReadLine();
+                        for (int y = 0; y < 5; y++)
+                        {
+                            Console.WriteLine("Customer Info");
+                            Console.WriteLine("No." + (y + 1) + (" Name: " + customers[y][0]) + (" Address: " + customers[y][1]) + (" Telephone: " + customers[y][2]));
+                        }
+                        Console.ReadLine();
+                    }
+                }
+
+
+
+
+            }
+        }
+    
+
+}
+    
+

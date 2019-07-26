e42d76625dc96a4dd4b5c8e2f7acdd4e923e1007
piyawat sairod <petturnj999@gmail.com>
2019-07-26 20:37:37 +0700

    addpro2

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1 ;
+            int b = int.Parse(Console.ReadLine());
+            for (i = 1; i <= 12; i++)
+            {
+                Console.WriteLine(b+"x"+i+"="+i*b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
